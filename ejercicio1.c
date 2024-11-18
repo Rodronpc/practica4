9e5bfd8cfa19af99400b2dec78f806c6e24f019a
Rodronpc <rodrigogarcia150105@gmail.com>
2024-11-18 01:16:26 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,63 @@
+/*Garcia Juarez Rodrigo
+Ejercici 1
+Hacer un codigo que permita elegir entre 4 operaciones basicas matematicas
+y poder realizarlas*/
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main ()
+{
+    int a = 0, b = 0, operation = 0;
+    int option;
+
+    printf("Operacion a realizar: \n");
+    printf("1 .- Suma\n");
+    printf("2 .- Resta\n");
+    printf("3 .- Mult\n");
+    printf("4 .- Div\n");
+    scanf("%d", &option);
+
+    
+
+    do
+    {
+        printf("Dame los numeros:\n");
+        printf("numero 1: ");
+        scanf("%d", &a);
+        printf("numero 2: ");
+        scanf("%d", &b);
+
+        switch (option)
+        {
+            case 1:
+                operation = a + b;
+                break;
+            case 2:
+                operation = a - b;
+                break;
+            case 3:
+                operation = a * b;
+                break;
+            case 4:
+                operation = a / b;
+                break;
+        }
+
+        printf("El resultado de la operacion es: %d\n", operation);
+
+        printf("Deseas realizar otra operacion: \n");
+        printf("1 .- Suma\n");
+        printf("2 .- Resta\n");
+        printf("3 .- Mult\n");
+        printf("4 .- Div\n");
+        printf("deseas salir: \n");
+        printf("-1 .- Salir\n");
+        scanf("%d", &option);
+    }
+    while (option != -1);
+
+    return 0;
+}
